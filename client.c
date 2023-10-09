84b77f86e07cb4d7850843cbad8c415593f03332
xuanwuGG <yyf1093765946@163.com>
2023-10-09 09:36:34 +0800

    10/9

--- client.c
+++ client.c
@@ -25,7 +25,7 @@ int main(int argc,char *argv[])
    int cfd=socket(AF_INET,SOCK_STREAM,0);
    struct sockaddr_in serv_add;
    serv_add.sin_family=AF_INET;
-   serv_add.sin_port=htons(2116);
+   serv_add.sin_port=htons(1111);
    inet_pton(AF_INET,"127.0.0.1",&serv_add.sin_addr.s_addr);
    int ret=connect(cfd,(struct sockaddr *)&serv_add,sizeof(serv_add));
    if(ret==-1)
@@ -33,7 +33,7 @@ int main(int argc,char *argv[])
        sys_err("connect errror:");
    }
    char buf[BUFSIZ];
-   for(int i=0;i<5;i++)
+   for(;;)
    {
        write(cfd,"bipaopu\n",8);
        int size=read(cfd,buf,sizeof(buf));
